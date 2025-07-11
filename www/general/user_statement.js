41452718e4d0e5a38a03449ccb85987eae7e6021
BossFame <famoulogy@gmail.com>
2025-07-11 01:54:32 -0700

    done

--- www/general/user_statement.js
+++ www/general/user_statement.js
@@ -42,5 +42,8 @@ window.addEventListener('load', function(e) {
         xhr.open('POST','user_statement.php', true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(json);
+    }else{
+        alert("You're not logged in");
+        window.location = "login.html";
     }
 });
